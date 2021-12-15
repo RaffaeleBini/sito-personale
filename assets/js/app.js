fde6cefa593e479f5beb6570da3e122457a62c03
Raffaele <88720362+RaffaeleBini@users.noreply.github.com>
2021-12-15 08:35:32 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,7 +66,7 @@ window.onload = function() {
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
+        emailjs.sendForm('service_b4j7kvs', 'contact_form', this)
             .then(function() {
                 console.log('SUCCESS!');
             }, function(error) {
