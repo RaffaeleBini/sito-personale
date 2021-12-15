9a46172272259bcfc81365bf23a581afd9b53634
Raffaele <88720362+RaffaeleBini@users.noreply.github.com>
2021-12-15 08:21:21 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -61,7 +61,7 @@ var slideIndex = 1;
     })();
 
 window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
+    document.getElementById('contact_form').addEventListener('submit', function(event) {
         event.preventDefault();
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
